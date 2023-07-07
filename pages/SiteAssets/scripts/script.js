c8cc671213f8d723e9bed591474ba48f47f13ee3
Russo <rafamega@live.com>
2023-07-06 21:08:18 -0300

    Enter also sends prompt now.

--- pages/SiteAssets/scripts/script.js
+++ pages/SiteAssets/scripts/script.js
@@ -28,4 +28,28 @@ const init = () => {
   submitHandler();
 };
 
+function getSelected(info) {
+  console.log(info.selectionText + " is being explored.");
+  document.getElementById("prompt-cima").value = info.selectionText;
+}
+
+chrome.contextMenus.create({
+  title: "Explore: \"%s\"", 
+  contexts:["selection"], 
+  onclick: getSelected
+});
+
+var input = document.getElementById("prompt");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("sendPrompt").click();
+  }
+});
+
 window.onload = init();
