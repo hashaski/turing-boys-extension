e865363ddbcc7f705734b594c441a96f291b52a6
Casagrande578 <casagrande696@gmail.com>
2023-07-06 22:56:30 -0300

    Creating various button functions and animations

--- pages/SiteAssets/scripts/script.js
+++ pages/SiteAssets/scripts/script.js
@@ -11,21 +11,68 @@ import getAiResponse from "./api.js";
 //   });
 // });
 
+const AIResponse = document.getElementById("AIResponse");
+let undo = [];
+let redo;
+const sendButton = document.getElementById("sendPrompt");
+const spinner = document.getElementById("spinner");
+const sendImg = document.getElementById("sendImg");
+
 const submitPrompt = async () => {
-  const prompt = document.getElementById("prompt").value;
-  const response = await getAiResponse(prompt);
-  document.getElementById("AIResponse").value = response;
+  const prompt = document.getElementById("prompt");
+  sendImg.hidden = true;
+  spinner.hidden = false;
+  const response = await getAiResponse(prompt.value);
+  sendImg.hidden = false;
+  spinner.hidden = true;
+
+  AIResponse.value = response;
+  prompt.value = "";
+  undo.push(response);
+  undo.length > 2 ? undo.pop() : null;
 };
 
 const submitHandler = () => {
-  const sendButton = document.getElementById("sendPrompt");
   sendButton.addEventListener("click", async () => {
     await submitPrompt();
   });
 };
 
+const copyHandler = () => {
+  const copyButton = document.getElementById("copy");
+  copyButton.addEventListener("click", () => copyText());
+};
+
+const copyText = () => {
+  const text = document.getElementById("prompt-cima");
+
+  text.select();
+
+  navigator.clipboard.writeText(text.value);
+};
+
+const undoButtonHandler = () => {
+  document.getElementById("undoButton").addEventListener("click", () => {
+    if (!!undo.length) {
+      redo = AIResponse.value;
+      AIResponse.value = undo.shift();
+    }
+  });
+};
+
+const redoButtonHandler = () => {
+  document.getElementById("redoButton").addEventListener("click", () => {
+    undo.push(AIResponse.value);
+    undo.length > 2 ? undo.pop() : null;
+    AIResponse.value = redo;
+  });
+};
+
 const init = () => {
   submitHandler();
+  copyHandler();
+  undoButtonHandler();
+  redoButtonHandler();
 };
 
 window.onload = init();
